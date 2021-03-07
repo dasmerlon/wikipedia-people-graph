3013e97e765da2d857645d83643669a60a1b4df8
LeHenningo <46297681+LeHenningo@users.noreply.github.com>
2021-03-07 15:45:29 +0100

    Add files via upload
    Spring Vorlage von basecamp (https://git.informatik.uni-hamburg.de/base.camp/backend-server)  für den Webserver, mit Implementation von Dashboard Vorlage von Bootstrap (https://getbootstrap.com/docs/5.0/examples/).

---
+++ backend-server-master/src/main/java/basecamp/project/server/model/UserDBModel.java
@@ -0,0 +1,21 @@
+package basecamp.project.server.model;
+
+import basecamp.project.server.model.type.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDBModel implements UserModel {
+
+    @Override
+    public List<User> getUsers() {
+
+        List<User> users = new ArrayList<>();
+
+        // Get DB connection
+
+        // get list of users
+
+        return users;
+    }
+}
