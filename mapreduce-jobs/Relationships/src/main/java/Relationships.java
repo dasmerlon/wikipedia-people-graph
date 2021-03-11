4db751f5b09384d8b57aa76be8ffaf621ea608c9
Merle Hoffmann <hoffmannmerle@web.de>
2021-03-11 23:56:24 +0100

    Add missing comments to Relationships.

--- mapreduce-jobs/Relationships/src/main/java/Relationships.java
+++ mapreduce-jobs/Relationships/src/main/java/Relationships.java
@@ -6,7 +6,18 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /**
- *
+ * Die Klasse Relationships erzeugt ein Map-Only Job, der aus Personenartikel von Wikipedia den Artikelnamen
+ * und alle im Artikel erwähnten Personen zusammen mit dem Satz, in dem sie erwähnt werden, ausgibt.
+ * Dabei werden nur Personen beachtet, die auch selbst einen Wikipediaartikel haben.
+ * Der Map-Only Job erwartet einen Wikipedia-Dump als XML-Datei, der nur Personenartikel enthält. Dafür kann
+ * das Projekt PersonArticleExtractor verwendet werden.
+ * <p>
+ * Der Output enthält in jeder Zeile den Wikipediatitel der ersten Person, einen weiteren Wikipediatitel einer
+ * im Artikel erwähnten Person und den Satz, in dem die zweite Person erwähnt wurde.
+ * Diese Daten werden durch den Delimiter >>>> voneinander getrennt.
+ * Eine Zeile ist wie folgt aufgebaut:
+ * <p>
+ * PERSON1>>>>PERSON2>>>>SENTENCE
  */
 public class Relationships {
 
@@ -17,6 +28,9 @@ public class Relationships {
      * Mappers und des Input-Formats an. Außerdem legen wir hier fest, dass es keinen Reduce Job gibt.
      */
     public static void main(String[] args) throws Exception {
+
+        // Wir setzen in der Konfiguration fest, dass der Mapper-Input mit <page> beginnt und </page> endet.
+        // Somit bekommt der Mapper immer eine ganze Wikipediaseite als Input.
         Configuration conf = new Configuration();
         conf.set("xmlInput.start", "<page>");
         conf.set("xmlInput.end", "</page>");
@@ -24,10 +38,11 @@ public class Relationships {
         Job job = Job.getInstance(conf, "relationships");
         job.setJarByClass(Relationships.class);
 
+        // Der Mapper verwendet die Textdatei mit den Personennamen mithilfe von Hadoop DistributedCache.
+        // Wir geben hier den Pfad der Datei an, die zwischengespeichert werden soll.
         job.addCacheFile(new Path("hdfs:///user/7mhoffma/wikipedia-people-graph/data/PersonNames.txt").toUri());
 
         job.setMapperClass(RelationshipMapper.class);
-        //job.setReducerClass(InformationReducer.class);
         job.setInputFormatClass(XmlInputFormat.class);
         job.setNumReduceTasks(0);
 
@@ -38,6 +53,5 @@ public class Relationships {
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
-
     }
 }
