997b630646ccb62ac0cad0d8ed64def716352fa8
LeHenningo <46297681+LeHenningo@users.noreply.github.com>
2021-03-12 16:29:15 +0100

    Presentation Version Visualisation working. second Layer implemented but no time tested yet, so outcommented at the moment.

--- backend-server-master-networkgraph/src/main/java/basecamp/project/server/controller/FilterController.java
+++ backend-server-master-networkgraph/src/main/java/basecamp/project/server/controller/FilterController.java
@@ -1,14 +1,20 @@
 package basecamp.project.server.controller;
 
-
+//import org.json.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+//import org.json.JSONObject;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uhh_lt.datenbank.MySQLconnect;
+
+import java.lang.reflect.Array;
 //import uhh_lt.datenverarbeitung.Verarbeitung; //vermutlich nur daten ummodeln
 
 
@@ -23,7 +29,7 @@ public class FilterController {
 
 	//TODO defaultValue ggf. anpassen
 
-	public String users(@RequestParam(value = "person", required=false, defaultValue = "") String person) throws JSONException {
+	public String users(@RequestParam(value = "person", required=false, defaultValue = "") String person) throws JSONException, ParseException {
 
 		MySQLconnect con = new MySQLconnect();
 		System.out.println("Connector erstellt");
@@ -32,16 +38,21 @@ public class FilterController {
 
 		String jsonout = con.getWatson(person);
 
-
-
-
+		String jsonProcessed = converter(jsonout);
 
 	//Geburts und Todesdaten umformatieren
 
 
 		//String formattedjsonout = converter(jsonout);
 
-		return jsonout;
+
+
+
+
+
+		//return jsonProcessed;
+
+		return jsonProcessed;
 	}
 
 	@RequestMapping("nrusers")
@@ -51,69 +62,258 @@ public class FilterController {
 
 
 
-	// Example of jsonStr
-/* String jsonStr = "{\n"
-        + "\"dt1\":\"12-12-2020\",\n"
-        + "\"street\":\"test\",\n"
-        + "\"city\":\"test2\",\n"
-        + "\"country\":\"test3\",\n"
-        + "\"dt2\":\"12-11-2020\"\n"
-        + "}";
-*/
-	public String converter(String jsonStr) throws JSONException {
 
-		// jsonStr is our original JSON with the date in the dash format (e.g. "12-12-2020")
+	public String converter(String jsonStr) throws JSONException, ParseException {
 
-		JSONObject jo = new JSONObject();
-
-		// populate the array
-		jo.put("unsereAlteJson",jsonStr);
-		// Here we convert into our json object
-		//JSONObject jsono = new JSONObject(jsonStr);
-
-		//jsono.forEach((item) => item.BIRTH_DATE = item.optString("BIRTH_DATE").substring(3,dateStr.length()+1 ));
-
-	/*	// Get the orginal date value
-		for (int i = 0; i< jo.getJSONArray("unsereAlteJson").length(); i++ ) {
-
-			String dateStrB = (String) jo.getJSONArray("unsereAlteJson")[i].get("BIRTH_DATE");
-			String dateStrD = (String) jo.get("DEATH_DATE");
-
-			// Convert the date into new format and update the JSON key 'dt1' with the new value
-			jo.put("BIRTH_DATE", dateConverter(dateStrB));
-			jo.put("DEATH_DATE", dateConverter(dateStrD));
+		System.out.println("CONVERTER INPUT " + jsonStr);
 
 
-		}*/
+		JSONParser parser = new JSONParser();
+
+		JSONArray inputArray = (JSONArray) parser.parse(jsonStr);
+				//new JSONArray(jsonStr);
+
+		System.out.println("NACH PARSEN " + inputArray);
+
+		JSONArray nodesArray = new JSONArray();
+		JSONArray edgesArray = new JSONArray();
+		JSONArray contactPersonArray = new JSONArray();
+
+		//build nodes
+
+		for(Object o: inputArray){
+
+				JSONObject o2 = (JSONObject)o;
+
+				//String neueId1 = ((JSONObject) o).getString("PERSON2");
+				String neueId1 = (String) o2.get("PERSON2");
+				System.out.println("PERSON 2 ziehen" + (String) o2.get("PERSON2") + neueId1 );
+				JSONObject node1 = new JSONObject();
+
+				node1.put("id", neueId1);
+
+				System.out.println("node1 Objekt" + node1);
+
+				nodesArray.add(node1);
+
+				contactPersonArray.add(neueId1);
+
+				String neueId2 = (String) o2.get("PERSON1");
+				JSONObject node2 = new JSONObject();
+				node2.put("id", neueId2);
+
+				nodesArray.add(node2);
 
 
-/*
-		for (int i=0; i < jsono.length(); i++){
-			JSONObject itemArr = (JSONObject)jsono.get(i);
-			if(itemArr.get("IDSERV").getAsString().equals("2")){
-				itemArr.put("STATUSUPDATE", 1);
-			}else if(itemArr.get("IDSERV").getAsString().equals("3")){
-				itemArr.put("STATUSUPDATE", 2);
+		}
+
+		System.out.println("NODE ARRAY " + nodesArray);
+
+
+		//build edges
+
+		for(Object o: inputArray){
+
+			JSONObject o2 = (JSONObject)o;
+
+			//String neueId1 = ((JSONObject) o).getString("PERSON2");
+			String neueId1 = (String) o2.get("PERSON2");
+			String neueId2 = (String) o2.get("PERSON1");
+			JSONObject edge = new JSONObject();
+			edge.put("from", neueId1);
+			edge.put("to", neueId2);
+
+			edgesArray.add(edge);
+
+		}
+
+		JSONObject obj = new JSONObject();
+
+		obj.put("nodes", nodesArray);
+
+		obj.put("edges", edgesArray);
+
+
+		System.out.println("JSON OBJECT " + obj);
+		System.out.println("JSON OBJECT TO STRING " + obj.toString());
+
+		//JSONObject out = getContactPersons(obj);
+
+		return obj.toString() ;
+
+	}
+
+
+	public JSONObject getContactPersons(JSONObject jsonStr) throws JSONException, ParseException { //INPUT JSON DIREKT AUS DB
+		System.out.println("CONVERTER INPUT " + jsonStr);
+		JSONObject layer0Output = jsonStr;
+
+		String newjsonStr = jsonStr.toString();
+
+		JSONParser parser = new JSONParser();
+
+		JSONArray inputArray = (JSONArray) parser.parse(newjsonStr);
+		//new JSONArray(jsonStr);
+
+		System.out.println("NACH PARSEN " + inputArray);
+
+		JSONArray contactPersonArray = new JSONArray();
+
+		//build nodes
+
+		for(Object o: inputArray){
+
+			JSONObject o2 = (JSONObject)o;
+
+			//String neueId1 = ((JSONObject) o).getString("PERSON2");
+			String neueId1 = (String) o2.get("PERSON2");
+			System.out.println("PERSON 2 ziehen" + (String) o2.get("PERSON2") + neueId1 );
+
+			JSONObject contactObj = new JSONObject();
+			contactObj.put("ContactName", neueId1);
+
+			contactPersonArray.add(contactObj);
+
+		}
+
+
+		JSONObject out = getSecondLayer(contactPersonArray, layer0Output);
+		return (out);
+
+	}
+
+
+
+	public JSONObject getSecondLayer(JSONArray nodesLayer1, JSONObject layer0Ausgabe) throws JSONException, ParseException //INPUT CONTACT PERSON ARRAY
+	{
+		MySQLconnect con = new MySQLconnect();
+		System.out.println("Connector erstellt");
+
+		JSONArray alleDatenLayer2 = new JSONArray();
+
+		for(Object o: nodesLayer1)
+			{
+				JSONObject o2 = (JSONObject)o;
+				String nextPerson = (String) o2.get("ContactName");
+
+				String jsonout = con.getWatson(nextPerson); //hole Beziehungen aus DB
+				String jsonProcessed = converter(jsonout); //get Objekt mit nodes Array und egdes Array
+				alleDatenLayer2.add(jsonProcessed);
 			}
-		}*/
 
-		return jo.toString();
+
+		JSONArray allNodes = new JSONArray();
+		JSONArray allEdges = new JSONArray();
+
+		for(Object o: alleDatenLayer2) //alleDatenLayer2 IST ZUSAMMENGEFUEGT AUS AUS CONVERTER OUTPUTS
+		{
+			JSONObject o2 = (JSONObject)o;
+			JSONArray nodesInO = (JSONArray) o2.get("nodes");
+			JSONArray edgesInO = (JSONArray) o2.get("edges");
+
+			for(Object p: nodesInO)
+			{
+				String nextPerson = (String) o2.get("id");
+
+				JSONObject lulu = new JSONObject();
+				lulu.put("id", nextPerson);
+				allNodes.add(lulu);
+			}
+
+			for(Object p: edgesInO)
+			{
+				String fromPerson = (String) o2.get("from");
+				String toPerson = (String) o2.get("to");
+
+				JSONObject lala = new JSONObject();
+				lala.put("from", fromPerson);
+				lala.put("to", toPerson);
+				allEdges.add(lala);
+			}
+
+		}
+
+		JSONObject moddedjson = new JSONObject();
+		moddedjson.put("nodes", allNodes);
+		moddedjson.put("edges", allEdges);
+
+		JSONObject out = MergeLayer0AndLayer1(moddedjson,layer0Ausgabe);
+
+		return (out);
+
+		//     alleDatenLayer2 = [ { nodes:[{},{},{}], edges:[{},{},{}] } ,    { nodes:[{},{},{}], edges:[{},{},{}] } ]
+
+		//            = { allnodes:[{},{},{}], alledges:[{},{},{}] }
+
+
+
+
+
 	}
 
-	private String dateConverter(String dateStr) {
-		if (dateStr.equals("NONE"))
+	public JSONObject MergeLayer0AndLayer1(JSONObject NodesEdgesLayer1, JSONObject  NodesEdgesLayer0) throws JSONException, ParseException
+	{
+		// INPUT NodesEdgesLayer1 = { nodes:[{},{},{}], edges:[{},{},{}] }
+		// INPUT NodesEdgesLayer0 = { nodes:[{},{},{}], edges:[{},{},{}] }
+
+		JSONArray nodesComplete = new JSONArray();
+		JSONArray edgesComplete = new JSONArray();
+
+		JSONObject o2 = (JSONObject)NodesEdgesLayer0;
+		JSONArray nodesInLayer0 = (JSONArray) o2.get("nodes");
+		JSONArray edgesInLayer0 = (JSONArray) o2.get("edges");
+
+		JSONObject o3 = (JSONObject)NodesEdgesLayer1;
+		JSONArray nodesInLayer1 = (JSONArray) o3.get("nodes");
+		JSONArray edgesInLayer1 = (JSONArray) o3.get("edges");
+
+		for(Object p: nodesInLayer0)
 		{
-			return "";
-		}
-		else
-		{
-			String neuesDatum = dateStr.substring(3,dateStr.length()+1 );
-			return neuesDatum;
+			String nextPerson = (String) o2.get("id");
+
+			JSONObject lulu = new JSONObject();
+			lulu.put("id", nextPerson);
+			nodesComplete.add(lulu);
 		}
 
+		for(Object p: edgesInLayer0)
+		{
+			String fromPerson = (String) o2.get("from");
+			String toPerson = (String) o2.get("to");
+
+			JSONObject lala = new JSONObject();
+			lala.put("from", fromPerson);
+			lala.put("to", toPerson);
+			edgesComplete.add(lala);
+		}
+
+		for(Object p: nodesInLayer1)
+		{
+			String nextPerson = (String) o2.get("id");
+
+			JSONObject lulu = new JSONObject();
+			lulu.put("id", nextPerson);
+			nodesComplete.add(lulu);
+		}
+
+		for(Object p: edgesInLayer1)
+		{
+			String fromPerson = (String) o2.get("from");
+			String toPerson = (String) o2.get("to");
+
+			JSONObject lala = new JSONObject();
+			lala.put("from", fromPerson);
+			lala.put("to", toPerson);
+			edgesComplete.add(lala);
+		}
+
+		JSONObject out = new JSONObject();
+		out.put("nodes", nodesComplete);
+		out.put("edges", edgesComplete);
+
+		return (out);
 	}
 
-
-
+    // users --> getWatson --> Converter --> ContactPerson --> getSecondLayer --> MergeLayer0AndLayer1 --> return to users
 }
 
